ca778c6c7c69ca59c5d03c3ca1a2336838bf0900
vmalaviya <varunmalaviya7@gmail.com>
2023-06-29 18:53:59 -0400

    added styling and icons to navbar. Added navbar component to homepage page. Working on adding a toggleable  mobile menu next.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,42 @@
+import { Search, DarkMode, LightMode, Notifications, Menu, Close } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+
 function Navbar() {
     return (
-        <div>Navbar</div>
+        <div
+            className="flex items-center justify-between py-3 px-10"
+        >
+            <div
+                className="font-marker text-4xl"
+            >
+                Dost
+            </div>
+            <div>
+                <input 
+                    type="text" 
+                    placeholder="Search..." 
+                    className="bg-slate-200 text-gray-500 py-2 px-8 rounded-lg mx-3"
+                />
+                <Search />
+            </div>
+            <div>
+                <IconButton>
+                    <DarkMode />
+                </IconButton>
+                <IconButton>
+                    <LightMode />
+                </IconButton>
+                <IconButton>
+                    <Notifications />
+                </IconButton>
+                Varun Malaviya
+                <button
+                    className="bg-red-500 py-2 px-6 rounded-lg text-white ml-3"
+                >
+                    Logout
+                </button>
+            </div>
+        </div>
     )
 }
 
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,7 +1,9 @@
+import Navbar from "../components/Navbar"
+
 function Homepage() {
     return (
         <div>
-            Homepage
+            <Navbar/>
         </div>
     )
 }
