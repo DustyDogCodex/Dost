1dfd9eeffcd7a4f1ef23b152f18d9691f3f5326b
vmalaviya <varunmalaviya7@gmail.com>
2023-06-29 15:49:42 -0400

    added dependenices and material ui to work on styling the front-end. Setup more routes in App.jsx. Added boilerplate for pages and components to be added.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css'
+import Homepage from './pages/Homepage'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import UserProfile from './pages/UserProfile'
 
 function App() {
 
@@ -16,6 +18,14 @@ function App() {
                     <Route 
                         path='/register'
                         element={<Register />}
+                    />
+                    <Route 
+                        path='/homepage'
+                        element={<Homepage />}
+                    />
+                    <Route 
+                        path='/profile/:userId'
+                        element={<UserProfile />}
                     />
                 </Routes>
             </BrowserRouter>
