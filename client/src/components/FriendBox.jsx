fa1f3e0a2d02cfe363f6fcba6f378f549d617dbd
vmalaviya <varunmalaviya7@gmail.com>
2023-07-11 17:41:04 -0400

    added add / remove user icons to friend component. Instead of being displayed in a widget, friendBox will now be on the top of every post, letting users add people to their friendsList more easily. Setup some basic styling for post component.

--- client/src/components/FriendBox.jsx
+++ client/src/components/FriendBox.jsx
@@ -1,5 +1,7 @@
-/* this component will display information about a user's friends in a widget on the user's homepage.  */
+/* this component will display information about the user that created a post. This component will allow a user to add or remove another user depending on whether or not they are already in the user's friendsList */
 import axios from "axios"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faUserMinus, faUserPlus } from "@fortawesome/free-solid-svg-icons"
 
 function FriendBox({ friendId, userName, userProfilePic, status }) {
     
@@ -24,6 +26,18 @@ function FriendBox({ friendId, userName, userProfilePic, status }) {
                 <h5>{userName}</h5>
                 <p className="text-sm">{status}</p>
             </div>
+            {friendOrNah 
+                ?   <FontAwesomeIcon 
+                        icon={faUserMinus} 
+                        style={{color: "#06b0f9",}}
+                        onClick={() => addFriend}
+                    />
+                :   <FontAwesomeIcon 
+                        icon={faUserPlus} 
+                        style={{color: "#06b0f9",}} 
+                        onClick={() => addFriend}
+                    /> 
+            }
         </div>
     )
 }
--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,7 +1,23 @@
-function Post() {
+import FriendBox from "./FriendBox"
+
+function Post({ postId, postUserId, userName, location, description, imagePath, userProfilePic, likes, comments }) {
+    
     return (
         <div>
-            Post
+            <FriendBox 
+                friendId={postUserId}
+                name={userName}
+                status={location}
+                userProfilePic={userProfilePic}
+            />
+            <p>{description}</p>
+            {imagePath && (
+                <img 
+                    src={`${imagePath}`} 
+                    alt="post image"
+                    className="w-full h-auto" 
+                />
+            )}
         </div>
     )
 }
