6b7c890b073b5616fa556ba2c50f7a3291f49a5b
vmalaviya <varunmalaviya7@gmail.com>
2023-07-11 18:25:00 -0400

    set up a basic format for userProfile page. This will be very similar to the user Homepage except that it will not have creatPostWidget and friendsList.

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,7 +1,39 @@
+import axios from "axios"
+import { useEffect } from "react"
+import { useState } from "react"
+import { useParams } from "react-router-dom"
+import PostsDisplay from "../components/PostsDisplay"
+
 function UserProfile() {
+    //grabbing userId for requested profile page from params
+    const { userId } = useParams()
+
+    //state variable for storing fetched user information
+    const [ user, setUser ] = useState(null)
+
+    //making an API call to fetch users data from our database
+    useEffect(() => {
+        const getUserInfo = async() => {
+            axios.get(`http://localhost:5000/user/${userId}`)
+            .then(res => setUser(res.data))
+            .catch(err => console.log(err))
+        }
+        getUserInfo()
+    }, [])
+
+    if(!user){
+        return null
+    }
+
     return (
         <div>
-            UserProfile
+            <Navbar/>
+            <div
+                className="flex"
+            >
+                <UserProfileWidget />
+                <PostsDisplay />
+            </div>
         </div>
     )
 }
