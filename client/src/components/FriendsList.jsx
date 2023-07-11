8adca76b11b397e152a16713082d8ff9b0431bc1
vmalaviya <varunmalaviya7@gmail.com>
2023-07-11 18:16:17 -0400

    added a small friendsList component to display a list of user's friends

---
+++ client/src/components/FriendsList.jsx
@@ -0,0 +1,34 @@
+/* small widget that will display a list of the user's friends */
+
+import axios from "axios"
+import { useState } from "react"
+import FriendBox from "./FriendBox"
+
+function FriendsList() {
+    //state variable to store friendsList
+    const [ friends, setFriends ] = useState([])
+
+    //get request to get an up-to date friendsList including any recent additions/deletions by the user
+    axios.get(`http://localhost:5000/user/${userId}/friends`)
+    .then(res => setFriends(res.data))
+    .catch(err => console.log(err))
+
+    return (
+        <div>
+            <h3>Friend's List</h3>
+            <div>
+                {friends.map(friend => {
+                    <FriendBox 
+                        key={friend._id}
+                        friendId={friend._id}
+                        userName={`${firstName} ${lastName}`}
+                        status={friend.status}
+                        userProfilePic={friend.userProfilePic}                    
+                    />
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default FriendsList
