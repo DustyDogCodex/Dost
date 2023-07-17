a9b5b9dd323ff375a1c3e3a0a1aeffcee2b8eeef
vmalaviya <varunmalaviya7@gmail.com>
2023-07-17 12:37:31 -0400

    added styling text and images to advert component. Added a small array containing links to images and a brief description for advertisements. Each time the page renders a new item will randomly selected for display from the adverts array. Clicking on the image will redirect to the source of the images in a new tab.

--- client/src/components/Advert.jsx
+++ client/src/components/Advert.jsx
@@ -1,9 +1,48 @@
 function Advert() {
+    //array of advert images and descriptions
+    const adverts = [
+        {
+            image:'uncomfortable_watering_can.jpg',
+            description:'Tired of running out of water while watering your plants? With our newest invention that won\'t be a problem anymore!' 
+        },
+        {
+            image:'uncomfortable_boots.jpg',
+            description:'Ever been torn between avoiding the rain while also wanting to soak yourself in it? With our newest line of boots you can do both!' 
+        },
+        {
+            image:'engagement_mugs.jpg',
+            description:'Why drink from one mug when you can drink from two?! Double the coffee means double the existensial crises!' 
+        },
+        {
+            image:'chain_fork.jpg',
+            description:'Tired of how monotonous eating food can be? Our newest invention will revolutionise this millenium old boring way of eating by turning it into a true challenge!' 
+        }
+    ]
+
+    //selecting a random advertisement from the adverts array to display
+    const selectedAdvert = adverts[Math.floor(Math.random() * 4)]
+
     return (
         <div
-            className="m-3 p-3 bg-white rounded-lg w-full dark:bg-slate-800 dark:text-white"
+            className="m-3 p-3 bg-white rounded-lg w-[350px] dark:bg-slate-800 dark:text-white"
         >
-            Advert
+            <h5
+                className="text-center text-sm text-gray-500"
+            >
+                Advertisement
+            </h5>
+            <a href="https://www.theuncomfortable.com/" target="_blank">
+                <img 
+                    src={`./public/${selectedAdvert.image}`} 
+                    alt={selectedAdvert.image} 
+                    className="rounded-lg my-2"
+                />
+            </a>
+            <p
+                className="text-sm text-center"
+            >
+                {selectedAdvert.description}
+            </p>
         </div>
     )
 }
