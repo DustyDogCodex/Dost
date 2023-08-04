e17819f57f34a733902191d311e89ddb4b3078ca
vmalaviya <varunmalaviya7@gmail.com>
2023-08-04 16:45:44 -0400

    fixed typo. Accidentally wrote the build folder as 'build' instead of 'dist'.

--- server/index.js
+++ server/index.js
@@ -65,7 +65,7 @@ app.use(express.static(path.join(__dirname, 'dist')))
 
 app.get('/*', (req,res) => {
     res.sendFile(
-        path.join(__dirname, 'build', 'index.html'),
+        path.join(__dirname, 'dist', 'index.html'),
         function(err){
             if(err){
                 res.status(500).send(err)
