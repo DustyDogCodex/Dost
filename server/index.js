86626d2a47110003392fe3c7b630cf383b24f9ae
vmalaviya <varunmalaviya7@gmail.com>
2023-08-06 12:43:50 -0400

    updated cors options

--- server/index.js
+++ server/index.js
@@ -29,6 +29,8 @@ const app = express()
 app.use(cors(
     {
         origin: ['https://stupendous-meerkat-ae0dee.netlify.app', 'http://localhost:5173'],
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+        allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
         credentials: true 
     }
 ))
