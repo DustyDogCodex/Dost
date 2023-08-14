cb3ee56efddb1d3d957917df7ab7497814ebb927
vmalaviya <varunmalaviya7@gmail.com>
2023-08-14 19:20:00 -0400

    added a running pikachu loading animation while posts are loaded on homepage.

--- client/src/components/PostsDisplay.jsx
+++ client/src/components/PostsDisplay.jsx
@@ -1,12 +1,15 @@
 /* this component will be used to display posts for both the user homepage and user profile page. On the homepage, it will display all available posts. On the user profile page, it will only display posts from that particular user. */
 import axios from "axios"
-import { useState } from "react"
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
 import Post from "./Post"
+import pikachu from "../assets/pikachu.gif"
 
 function PostsDisplay({ userId, profile }) {
     //state variable for storing all fetched posts to display on userfeed
     const [ posts, setPosts ] = useState([])
+
+    //toggle for loading animation while data is fetched from server
+    const [ loading, setLoading ] = useState(true)
     
     //function to fetch all posts from server
     //this will be called if component is present in a user homepage
@@ -20,7 +23,10 @@ function PostsDisplay({ userId, profile }) {
     //this will be called if component is present in the user profile page
     const getUserPosts = async() => {
         axios.get(`https://dost-production.up.railway.app/posts/${userId}`)
-        .then(res => setPosts([ ...res.data ]))
+        .then(res => { 
+            setPosts([ ...res.data ])
+            setLoading(false)
+        })
         .catch(err => console.log(err))
     } 
 
@@ -34,8 +40,24 @@ function PostsDisplay({ userId, profile }) {
         <div
             className="p-2 m-2 flex flex-col items-center justify-center"
         >
-            {posts?.length == 0 
-                ? 
+            {loading 
+                ?
+                /* loading screen with a running pikachu animation :) */
+                (
+                    <div 
+                        className="w-full h-full flex justify-center items-center"
+                    >
+                        <img 
+                            src={pikachu}
+                            alt="pikachu running loading animation" 
+                            className="w-60 h-60"
+                        />
+                    </div>
+                )
+                :
+                <>
+                {posts.length == 0 
+                    ? 
                     <div>
                         <p
                             className="dark:text-white"
@@ -43,7 +65,7 @@ function PostsDisplay({ userId, profile }) {
                             User has not created any posts yet.
                         </p>
                     </div>
-                : 
+                    : 
                     posts.map(post => 
                         <Post 
                             key={post._id}
@@ -59,6 +81,8 @@ function PostsDisplay({ userId, profile }) {
                             comments={post.comments}   
                         />
                     ).reverse()
+                }
+                </>
             }
         </div>
     )
